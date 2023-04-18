132fd4e4945fa5c237a8f650b538cf68a6ce8291
Excell-09 <excell.agcb12@gmail.com>
2023-04-18 21:53:54 +0700

    create functionality likes

--- backend/src/controllers/likeController.ts
+++ backend/src/controllers/likeController.ts
@@ -1,11 +1,51 @@
 import { Request, Response } from 'express';
+import Post from '../models/post';
+import User from '../models/user';
+
+type TRequestLike = { email: string };
 
 export const getLikes = async (req: Request, res: Response) => {
   res.send('get likes');
 };
+
 export const like = async (req: Request, res: Response) => {
-  res.send('like');
+  const { email }: TRequestLike = req.body;
+  const { id } = req.params;
+
+  if (!email) {
+    return res.status(400).json('need Authentication');
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(400).json('user not found!');
+  }
+
+  const post = await Post.findById(id);
+  await post?.likes.push({ userId: user._id });
+
+  return res.status(200).json(post);
 };
 export const dislike = async (req: Request, res: Response) => {
-  res.send('dislike');
+  const { email }: TRequestLike = req.body;
+  const { id } = req.params;
+
+  if (!email) {
+    return res.status(400).json('need Authentication');
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(400).json('user not found!');
+  }
+
+  const post = await Post.findById(id);
+  const indexLikes = await post?.likes.findIndex((item) => item.userId === user._id);
+  if (indexLikes) {
+    await post?.likes.splice(indexLikes, 1);
+  }
+
+  return res.status(200).json(post);
 };
--- backend/src/routes/likeRoutes.ts
+++ backend/src/routes/likeRoutes.ts
@@ -3,6 +3,7 @@ const router = express.Router();
 
 import { getLikes, like, dislike } from '../controllers/likeController';
 
-router.route('/').get(getLikes).post(like).put(dislike);
+router.route('/').get(getLikes);
+router.route('/:id').post(like).put(dislike);
 
 export default router;
